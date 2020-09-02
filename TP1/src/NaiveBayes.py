91b5b567c7152635499f1ce76246cb4908bcd02b
Miguel <mdiluca@itba.edu.ar>
2020-09-01 23:03:32 -0300

    fix

--- TP1/src/NaiveBayes.py
+++ TP1/src/NaiveBayes.py
@@ -23,14 +23,17 @@ class NaiveBayes:
 
                 if possible_values is None:
                     occurrences_map = {0: 0, 1: 0}
+                    self.general_probabilities[i] = {0: 0, 1: 0}
                 else:
                     occurrences_map = {}
                     if isinstance(possible_values[0], list):
                         for value in possible_values[i]:
                             occurrences_map[value] = 0
+                            self.general_probabilities[i] = 0
                     else:
                         for value in possible_values:
                             occurrences_map[value] = 0
+                            self.general_probabilities[i] = 0
 
                 for item in dataset[dataset.columns[i]]:
                     if item in occurrences_map:
