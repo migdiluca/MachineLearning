c003ddc7cd1afa368c51875e0155977f89d9b23f
Miguel <mdiluca@itba.edu.ar>
2020-11-30 23:15:39 -0300

    update

--- TP4/src/main/java/src/kmeans/KMeansRunner.java
+++ TP4/src/main/java/src/kmeans/KMeansRunner.java
@@ -2,16 +2,17 @@ package src.kmeans;
 
 import src.utils.math.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 public class KMeansRunner {
-    public static KMeansInstance runKMeansSimulation(List<Vector> data, int runs) {
+    public static KMeansInstance runKMeansSimulation(List<Vector> data, int runs, int k) {
         KMeansInstance lowestKMean = null;
 
         double lowestVariance = Double.MAX_VALUE;
         for (int i = 0; i < runs; i++) {
-            KMeansInstance kMeansInstance = new KMeansInstance(2);
+            KMeansInstance kMeansInstance = new KMeansInstance(k);
             kMeansInstance.train(data);
             double variance = kMeansInstance.getVariance();
             if(variance < lowestVariance) {
@@ -20,5 +21,16 @@ public class KMeansRunner {
             }
         }
         return lowestKMean;
+    }
+
+    public static List<KMeansInstance> runKMeansGroupSimulation(List<Vector> data, int runs, int k) {
+        List<KMeansInstance> resp = new ArrayList<>(runs);
+
+        for (int i = 0; i < runs; i++) {
+            KMeansInstance kMeansInstance = new KMeansInstance(k);
+            kMeansInstance.train(data);
+            resp.add(kMeansInstance);
+        }
+        return resp;
     }
 }
